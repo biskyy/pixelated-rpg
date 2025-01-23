742d82216f86cb0ce92ab478acbfd0a2c3dc4bd1
biskyy <54770799+biskyy@users.noreply.github.com>
2025-01-23 23:44:40 +0200

    feat: implement qol methods on Vector3 class

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public static class Utilities
+{
+  public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+  {
+    return new Vector3(
+      x ?? vector.x,
+      y ?? vector.y,
+      z ?? vector.z
+    );
+  }
+
+  public static Vector3 Add(this Vector3 vector, float? x = null, float? y = null, float? z = null)
+  {
+    return new Vector3(
+      vector.x + (x ?? 0),
+      vector.y + (y ?? 0),
+      vector.z + (z ?? 0)
+    );
+  }
+}
